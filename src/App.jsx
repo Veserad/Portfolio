39f1d59aa1512ef38a67a7a79764309839c8a2f5
Sergio <ruizdiaz_sergio@outlook.com>
2025-08-28 14:08:15 -0300

    arreglos 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./Pages/Home";
 import Footer from "./Pages/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Home />
--- src/Pages/Diplomas/index.jsx
+++ src/Pages/Diplomas/index.jsx
@@ -1,10 +1,21 @@
-function Diploma({ imgSrc, pdfSrc, alt }) {
+function Diploma({
+  imgSrc,
+  pdfSrc,
+  alt,
+  style = {
+    "@media (max-width: 1023px)": {
+      width: "100px",
+      cursor: "pointer",
+      borderRadius: "8px",
+    },
+  },
+}) {
   return (
     <a href={pdfSrc} target="_blank" rel="noopener noreferrer">
       <img
         src={imgSrc}
         alt={alt}
-        style={{ width: "200px", cursor: "pointer", borderRadius: "8px" }}
+        style={{ width: "150px", cursor: "pointer", borderRadius: "8px" }}
       />
     </a>
   );
